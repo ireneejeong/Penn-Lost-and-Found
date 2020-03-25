56fd6d45f60f10c92f1e095d49ecf0dc8d11cfe6
Min Seok Kim <minskim0327@gmail.com>
2020-03-25 11:51:00 +0900

    first android commit

---
+++ Android/app/src/main/java/edu/upenn/cis350/lostandfound/MainActivity.java
@@ -0,0 +1,15 @@
+package edu.upenn.cis350.lostandfound;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
